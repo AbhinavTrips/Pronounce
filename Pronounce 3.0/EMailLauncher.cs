758bd69e365ab751e753c458a033d890673192e1
AbhinavDAIICT <abhinavtripathi01@hotmail.com>
2015-06-04 20:57:40 +0530

    added EmailLauncher
    This class is used to launch email task which will be used for direct
    feedback from users.

---
+++ Pronounce 3.0/EMailLauncher.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Phone.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pronounce_3._0
+{
+    class EMailLauncher
+    {
+        public void mailLaunch(String subject, String msgBody, String recipientAddress, String cc, String bcc)
+        {
+            EmailComposeTask emailComposeTask = new EmailComposeTask();
+
+            emailComposeTask.Subject = subject;
+            emailComposeTask.Body = msgBody;
+            emailComposeTask.To = recipientAddress;
+            emailComposeTask.Cc = cc;
+            emailComposeTask.Bcc = bcc;
+
+            emailComposeTask.Show();
+        }
+    }
+}
